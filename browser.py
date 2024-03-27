e8535f35b00200e9f8844c8620ce1c649a2ccca2
NGS-Adryan <ngs.adryan@gmail.com>
2024-03-27 13:48:20 -0300

    Adicionando docstrings
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- browser.py
+++ browser.py
@@ -1,23 +1,27 @@
 """
-
+    Arquivo encarregador de fazer requisições e lidar com HTML.
 """
 
 from requests import get
 from bs4 import BeautifulSoup as soup
 
-URL_GOOGLE_FINANCE = 'https://www.google.com/finance/quote/'
 
+def get_price_cripto(cripto: str) -> dict[str, str]:
+    """Função usada para pegar o preço de uma criptomoeda passada por
+    parametro ex:
+    >>> get_price_cripto('BTC')
+    ... {"name": "Bitcoin (BTC / BRL)", "price": "300.000,00"}
 
-def get_price_cripto(tag: str) -> dict[str, str]:
-    RESPONSE = get(f"{URL_GOOGLE_FINANCE}{tag}-BRL?hl=pt")
+    :param cripto: str - Nome resumido da criptomoeda a ser pesquisada.
+    :return: dict[str, str] - Dicionário contendo o nome e o preço.
+    """
+    RESPONSE = get(f'https://www.google.com/finance/quote/{cripto}-BRL?hl=pt')
     SOUP = soup(RESPONSE.text, 'html.parser')
     price = SOUP.find(class_="YMlKec fxKbKc").text
     name = SOUP.find(class_="zzDege").text
-    return {"name": name, "price": f"R$ {price}"}
+    return {"name": name, "price": price}
 
 
 if __name__ == "__main__":
     price = get_price_cripto('BTC')
     print(price)
-
-    pass
